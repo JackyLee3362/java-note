a47599450ae605006a04ffec2521fecd217b324a
lijie206 <lijie206@meituan.com>
2025-06-30 19:49:22 +0800

    ✅test: 新增 junit5 模块 Basic 笔记

---
+++ junit5-note/src/main/java/note/junit/junit5/Calculator.java
@@ -0,0 +1,11 @@
+package note.junit.junit5;
+
+/**
+ * @author jackylee
+ * @date 2025/6/30 19:41
+ */
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
