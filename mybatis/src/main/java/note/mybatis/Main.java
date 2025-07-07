f5ed28014c90a0de26bd5eab8d26242213811488
lijie206 <lijie206@meituan.com>
2025-07-07 20:11:02 +0800

    🚧code: 新增模块

---
+++ mybatis/src/main/java/note/mybatis/Main.java
@@ -0,0 +1,12 @@
+package note.mybatis;
+
+/**
+ * @author jackylee
+ * @date 2025/7/7 09:33
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ spring-web/src/main/java/note/spring/StartApp.java
@@ -0,0 +1,16 @@
+package note.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jackylee
+ * @date 2025/7/7 09:19
+ */
+@SpringBootApplication
+public class StartApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+}
