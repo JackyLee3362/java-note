0d50846e3bb463ac932d0985fe91e4d6b0e7717b
lijie206 <lijie206@meituan.com>
2025-07-07 20:08:38 +0800

    ️📝(java-util): 新增 stream map 笔记

---
+++ java-util/src/main/java/note/java/model/User.java
@@ -0,0 +1,16 @@
+package note.java.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author jackylee
+ * @date 2025/7/7 14:17
+ */
+@AllArgsConstructor
+@Data
+public class User {
+
+    private String name;
+    private Integer age;
+}
---
+++ java-util/src/test/java/note/java/util/StreamTest.java
@@ -0,0 +1,42 @@
+package note.java.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import note.java.model.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author jackylee
+ * @date 2025/7/3 19:17
+ */
+public class StreamTest {
+
+    @Test
+    public void testStreamToList() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5");
+        List<Integer> collect = list.stream().map(Integer::parseInt).collect(Collectors.toList());
+        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5}, collect.toArray());
+    }
+
+    @Test
+    public void testStreamToMap() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Alice", 12));
+        users.add(new User("Bob", 13));
+        users.add(new User("Charlie", 14));
+        users.add(new User("David", 15));
+        users.add(new User("Alice", 16));
+        Map<String, Integer> map = users.stream().collect(
+            Collectors.toMap(
+                User::getName,
+                User::getAge,
+                (oldValue, newValue) -> newValue)
+        );
+        System.out.println(map);
+    }
+}
