b48766124626a3f82667ffee618b130b29355bd1
Jackylee <jieli.work@qq.com>
2025-07-26 08:35:30 +0800

    ✨feat(jwt): 新增

---
+++ spring-jwt/src/test/java/edu/note/spring/jwt/JwtUtilTest.java
@@ -0,0 +1,31 @@
+package edu.note.spring.jwt;
+
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class JwtUtilTest {
+    static String token;
+
+
+    @Test
+    void createJWT() {
+        long expired = System.currentTimeMillis() + 15 * 60 * 1000;
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", "123");
+        claims.put("name", "alice");
+        token = JwtUtil.createJWT("Jacky", expired, claims);
+        System.out.println("加密后的结果是："+token);
+    }
+
+    @Test
+    void parseJWT() {
+        if(token == null){
+            return;
+        }
+        Claims claims = JwtUtil.parseJWT("Jacky", token);
+        System.out.println("解析后的结果是："+claims);
+    }
+}
