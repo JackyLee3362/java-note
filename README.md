c7abd17b442b3a88922d15382ab5dd22d03504cf
Jackylee <jieli.work@qq.com>
2025-09-14 16:27:12 +0800

    ✨feat: 插件 enforcer 测试

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # README
 
 Java 代码笔记 - Today
+
+## vscode 开发指南
+
+## Java 编译器配置
+
+## Maven 配置
---
+++ maven-plugin-test/src/test/groovy/edu/note/SpockUnitSpec.groovy
@@ -0,0 +1,15 @@
+package edu.note
+
+import spock.lang.Specification
+
+/**
+ * @author jackylee
+ * @date 2025/9/12 20:25
+ */
+class SpockUnitSpec extends Specification{
+
+    def "test 1"(){
+        expect:
+        true
+    }
+}
