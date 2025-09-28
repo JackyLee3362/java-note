616fd902fe14ca7e6dfb0466c72a52e147612618
lijie206 <lijie206@meituan.com>
2025-09-28 15:07:39 +0800

    ➕deps: 增加lombok junit5 slf4j依赖

--- spring-pom/spring-mq/src/test/java/edu/note/spring/mq/producer/PublisherTest.java
+++ spring-pom/spring-mq/src/test/java/edu/note/spring/mq/producer/PublisherTest.java
@@ -3,10 +3,11 @@ package edu.note.spring.mq.producer;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import org.junit.Test;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+
+import org.junit.jupiter.api.Test;
 
 public class PublisherTest {
     @Test
--- spring-pom/spring-mq/src/test/java/edu/note/spring/mq/producer/SpringAmqpTest.java
+++ spring-pom/spring-mq/src/test/java/edu/note/spring/mq/producer/SpringAmqpTest.java
@@ -1,13 +1,11 @@
 package edu.note.spring.mq.producer;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringAmqpTest {
     @Autowired
